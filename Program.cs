9a6790e2e33aa4a7e12f41916643c19afc56a014
ladynazgool <ladynazgool@gmail.com>
2022-02-23 17:36:34 +0100

    it works:)))

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+ 
+string[] array =
+{
+    "a",
+    "ab",
+    "abc",
+    "abcc",
+    "ccfds",
+    "ssk"
+};
+ 
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+
